9179d6cca266cfecfc9a3837173182273e2cc9d8
s28346 <s28346@pjwstk.edu.pl>
2024-03-08 18:59:17 +0100

    hello

--- Lesson_1_2/Lesson_1_2/Program.cs
+++ Lesson_1_2/Lesson_1_2/Program.cs
@@ -5,3 +5,17 @@ Console.WriteLine("So the second project per day !");
 Console.WriteLine("So the  ");
 Console.WriteLine("second ");
 Console.WriteLine("project per day !");
+
+static double GetAverage(int[] nums)
+{
+    double sum = 0.0;
+    foreach (var n in nums)
+    {
+        sum += n;
+    }
+    return sum / nums.Length;
+}
+
+int[] nums={1,2,4,5,6,7};
+double avg=GetAverage(nums);
+Console.WriteLine(avg);
