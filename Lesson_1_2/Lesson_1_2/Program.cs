cd3f0602dbaabd595fb5bf86ffc2658eb0499ca4
s28346 <s28346@pjwstk.edu.pl>
2024-03-08 18:29:34 +0100

    Hello world

--- Lesson_1_2/Lesson_1_2/Program.cs
+++ Lesson_1_2/Lesson_1_2/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+Console.WriteLine("So the second project per day !");
+Console.WriteLine("So the  ");
+Console.WriteLine("second ");
+Console.WriteLine("project per day !");
